33843b60be129376c2025ec6606de9687b523481
Congcong Cai (EC-EE-JD-4) <congcong.cai@bmw.com>
2025-09-04 18:29:16 +0800

    chore(build): generate assemblyscript build result to CMAKE_BINARY_DIR (#96)

--- frontend/ASC/gen.mjs
+++ frontend/ASC/gen.mjs
@@ -3,7 +3,8 @@ import { readFileSync, writeFileSync } from "node:fs";
 import { join } from "node:path";
 import assert from "node:assert";
 
-const build_target = process.argv[2];
+const target_folder = process.argv[2];
+const build_target = process.argv[3];
 assert(build_target === "debug" || build_target === "release", "invalid build target");
 
 const project_root = join("..", "..");
@@ -12,20 +13,23 @@ execSync("node scripts/build.js", { cwd: join(project_root, "assemblyscript") })
 
 const { libraryFiles } = await import("../../assemblyscript/cli/index.generated.js");
 writeFileSync(
-  join(project_root, "build-as/library_sources.inc"),
+  join(target_folder, "library_sources.inc"),
   Object.keys(libraryFiles)
     .map((fileName) => `{\n  "${fileName}", R"##(${libraryFiles[fileName]})##",\n},\n`)
     .join("")
 );
 
-execSync(`npx asc --config asconfig.json --target ${build_target}`, { cwd: project_root });
+execSync(
+  `npx asc --config asconfig.json --target ${build_target} -o ${target_folder}/assemblyscript.${build_target}.wasm -t ${target_folder}/assemblyscript.${build_target}.wast`,
+  { cwd: project_root }
+);
 
-const wasmBuf = readFileSync(join(project_root, `build-as/assemblyscript.${build_target}.wasm`));
+const wasmBuf = readFileSync(join(target_folder, `assemblyscript.${build_target}.wasm`));
 const wasmBytes = Array.from(wasmBuf)
   .map((byte) => "0x" + byte.toString(16).padStart(2, "0"))
   .join(", ");
 writeFileSync(
-  join(project_root, "build-as/assemblyscript.inc"),
+  join(target_folder, "assemblyscript.inc"),
   `
 unsigned char asc_wasm[] = {${wasmBytes}};
 unsigned int asc_wasm_len = {${wasmBuf.length}};
